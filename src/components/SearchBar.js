bb9b97e22eb4d8f4f864d327d0c9cad84197e7d9
97joaquinhr <97joaquinhr@gmail.com>
2019-10-06 19:51:16 -0500

    Added search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { View,TextInput,StyleSheet } from 'react-native';
+import {AntDesign} from '@expo/vector-icons';
+
+const SearchBar = ({term, onTermChange,onTermSubmit}) =>{
+    return(
+        <View style={styles.backgroundStyle}>
+            <AntDesign name="search1" style={styles.iconStyles}/>
+            <TextInput 
+                style={styles.inputStyle}
+                placeholder = "Search"
+                value = {term}
+                autoCorrect = {false}
+                autoCapitalize = "none"
+                onEndEditing = {onTermSubmit}
+                onChangeText = {onTermChange}
+            />
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    backgroundStyle:{
+        marginTop:12,
+        backgroundColor: '#F0EEEE',
+        height: 45,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row',
+    },
+    inputStyle:{
+        flex: 1,
+        fontSize: 18,
+    },
+    iconStyles:{
+        fontSize:32,
+        alignSelf: 'center',
+        marginHorizontal: 15,
+    }
+});
+export default SearchBar;

fa3ff078cc93b90cddd73064c4bd61508c52791e
97joaquinhr <97joaquinhr@gmail.com>
2019-10-08 14:42:12 -0500

    Added scrollview

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,22 +1,42 @@
-import React,{useState, useEffect} from 'react';
-import {View,Text,StyleSheet } from 'react-native';
+import React,{useState} from 'react';
+import {View,Text,StyleSheet, ScrollView } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () =>{
     const[term,setTerm] = useState('');
     const [searchApi,results,errorMessage] = useResults();
+    
+    const filterResultsByPrice = (price)=>{
+        return results.filter(result => {
+            return result.price === price;
+        });
+    };
 
     return(
-        <View>
+        <>
             <SearchBar 
                 term = {term} 
                 onTermChange = {newTerm => setTerm(newTerm)}
                 onTermSubmit = { () => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text>: null}
-            <Text>We have found {results.length} results.</Text>
-        </View>
+            <ScrollView>
+                <ResultsList
+                    results={filterResultsByPrice('$')}
+                    title= "Cost effective"
+                />
+                <ResultsList
+                    results={filterResultsByPrice('$$')}
+                    title= "Bit pricier"
+                />
+                <ResultsList
+                    results={filterResultsByPrice('$$$')}
+                    title= "Big spender"
+                />
+            </ScrollView>
+        </>
     );
 };
 
